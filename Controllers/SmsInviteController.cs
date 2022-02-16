32283b7db1ca18f61c3da017193b1086ad35c614
Тимур <timovhinikov@gmail.com>
2022-02-16 21:56:09 +0300

    init project

---
+++ Controllers/SmsInviteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using sms_invite.Models;
+
+namespace sms_invite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SmsInviteController : ControllerBase
+    {
+        private readonly ILogger<SmsInviteController> _logger;
+
+        public SmsInviteController(ILogger<SmsInviteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public SmsInvite Invite(SmsInvite invite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ models/SmsInvite.cs
@@ -0,0 +1,8 @@
+namespace sms_invite.Models
+{
+    public class SmsInvite
+    {
+        public string[] Numbers { get; set; }
+        public string Message { get; set; }
+    }
+}
